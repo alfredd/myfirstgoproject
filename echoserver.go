e192440d8b54798a84cedbc125cdb978b6351f69
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2018-12-15 20:33:42 -0800

    Built a simple echo server and client.

--- echoserver.go
+++ echoserver.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"github.com/alfredd/myfirstgoproject/protocol"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
 
+type Server struct{}
+
+func (s *Server) Echo(ctx context.Context, e *echo.EchoMessage) (*echo.EchoMessage, error) {
+	return e, nil
+}
+
+func main() {
+	port := 4550
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
+	if err != nil {
+		log.Fatal("Error in listening to port")
+	}
+
+	server := grpc.NewServer()
+	echo.RegisterEchoServerServer(server, &Server{})
+	server.Serve(lis)
 }
