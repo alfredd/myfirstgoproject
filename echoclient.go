ce3569c5b70dee32e097df04417d2b9eaae7c93d
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2018-12-15 20:39:15 -0800

    Removing tabs for spaces.

--- echoclient.go
+++ echoclient.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"github.com/alfredd/myfirstgoproject/protocol"
-	"google.golang.org/grpc"
-	"log"
-	"time"
+    "context"
+    "fmt"
+    "github.com/alfredd/myfirstgoproject/protocol"
+    "google.golang.org/grpc"
+    "log"
+    "time"
 )
 
 func main() {
-	port := 4550
-	conn, err := grpc.Dial(fmt.Sprintf("localhost:%d", port), grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("Error occurred while connecting to server: %v", err)
-	}
-	defer conn.Close()
-	client := echo.NewEchoServerClient(conn)
+    port := 4550
+    conn, err := grpc.Dial(fmt.Sprintf("localhost:%d", port), grpc.WithInsecure())
+    if err != nil {
+        log.Fatalf("Error occurred while connecting to server: %v", err)
+    }
+    defer conn.Close()
+    client := echo.NewEchoServerClient(conn)
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+    defer cancel()
 
-	reply, err := client.Echo(ctx, &echo.EchoMessage{Message: "Whats up!"})
-	if err != nil {
-		log.Fatalf("Error occurred while sending message to server: %v", err)
-	}
-	log.Printf("Response from server: %s", reply.Message)
+    reply, err := client.Echo(ctx, &echo.EchoMessage{Message: "Whats up!"})
+    if err != nil {
+        log.Fatalf("Error occurred while sending message to server: %v", err)
+    }
+    log.Printf("Response from server: %s", reply.Message)
 
 }
--- echoserver.go
+++ echoserver.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"github.com/alfredd/myfirstgoproject/protocol"
-	"google.golang.org/grpc"
-	"log"
-	"net"
+    "context"
+    "fmt"
+    "github.com/alfredd/myfirstgoproject/protocol"
+    "google.golang.org/grpc"
+    "log"
+    "net"
 )
 
 type Server struct{}
 
 func (s *Server) Echo(ctx context.Context, e *echo.EchoMessage) (*echo.EchoMessage, error) {
-	return e, nil
+    return e, nil
 }
 
 func main() {
-	port := 4550
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
-	if err != nil {
-		log.Fatal("Error in listening to port")
-	}
+    port := 4550
+    lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
+    if err != nil {
+        log.Fatal("Error in listening to port")
+    }
 
-	server := grpc.NewServer()
-	echo.RegisterEchoServerServer(server, &Server{})
-	server.Serve(lis)
+    server := grpc.NewServer()
+    echo.RegisterEchoServerServer(server, &Server{})
+    server.Serve(lis)
 }
